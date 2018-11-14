82934e9afc8fbb5bfb45306467fb059866e92313
isychevskyi <arhell333@gmail.com>
2018-11-14 18:50:33 +0200

    upd function & arr

--- js-practice/tic-tac-toe/app.js
+++ js-practice/tic-tac-toe/app.js
@@ -1,40 +1,55 @@
-var cell = [
-  id = 1,
-  id = 2,
-  id = 3,
-  id = 4,
-  id = 5,
-  id = 6,
-  id = 7,
-  id = 8,
-  id = 9
+var cells = [
+  {id: 1},
+  {id: 2},
+  {id: 3},
+  {id: 4},
+  {id: 5},
+  {id: 6},
+  {id: 7},
+  {id: 8},
+  {id: 9}
 ];
 
-function makeCell() {
-  var listContainer = document.getElementsByClassName('wrapper')[0];
-  var listElement = document.createElement('ul');
-  listContainer.appendChild(listElement);
-  var numListItems = cell.length;
 
-  for (let i = 0; i < numListItems; ++i) {
-    var listItem = document.createElement('li');
-    listItem.innerHTML = cell[i];
-    listElement.appendChild(listItem);
-  }
+function makeCells() {
+  var сontainer = document.getElementsByClassName('wrapper')[0];
+  var element = document.createElement('ul');
+  сontainer.appendChild(element);
+
+  for(let i = 0; i < cells.length; i++) {
+      var item = document.createElement('li');
+      item.setAttribute('id', cells[i].id);
+      element.appendChild(item);
+    }
 }
 
-makeCell()
+makeCells()
 
-console.log(cell.length);
+// var сontainer = document.getElementsByClassName('wrapper')[0];
+// var element = document.createElement('ul');
+// сontainer.appendChild(element);
+//
+// cells.map(createCell)
+//
+// function createCell(item) {
+//   var el = document.createElement('li');
+//   el.setAttribute('id', item.id);
+//   element.appendChild(el);
+// }
+
 
 var liItem = document.getElementsByTagName('li');
-console.log(liItem)
+//console.log(liItem)
 
 for (let i = 0; i < liItem.length; i++) {
   liItem[i].addEventListener("click", currentStep);
 }
 
 function currentStep() {
-  console.log('!!!')
+  var playerOne = 'x';
+  var playerTwo = 'o';
+  this.innerHTML = playerOne
+  console.log(this)
 }
+
 
