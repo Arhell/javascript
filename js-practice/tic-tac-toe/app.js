6105aea7e2cf54f6ef63e83e3d243c477bb929c1
Arhell <arhell333@gmail.com>
2018-11-21 00:50:40 +0200

    upd checkwin

--- js-practice/tic-tac-toe/app.js
+++ js-practice/tic-tac-toe/app.js
@@ -1,13 +1,13 @@
 var cells = [
-  {id: 1},
-  {id: 2},
-  {id: 3},
-  {id: 4},
-  {id: 5},
-  {id: 6},
-  {id: 7},
-  {id: 8},
-  {id: 9}
+  {},
+  {},
+  {},
+  {},
+  {},
+  {},
+  {},
+  {},
+  {} // 9
 ];
 
 
@@ -18,7 +18,6 @@ function makeCells() {
 
   for(let i = 0; i < cells.length; i++) {
       var item = document.createElement('li');
-      item.setAttribute('id', cells[i].id);
       element.appendChild(item);
     }
 }
@@ -48,23 +47,8 @@ function currentStep() {
 }
 
 function checkWinner(getElement) {
-  var winConbinations = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-  ];
-
-  for (let i =0; winConbinations.length; i++) {
-    var win = winConbinations[i];
-
-    if ((getElement[win[0]]).innerHTML === (getElement[win[1]]).innerHTML &&
-        (getElement[win[1]]).innerHTML === (getElement[win[2]]).innerHTML &&
-        (getElement[win[0]]).innerHTML !== '') {alert('WIN!!')}
+  for (let i =0; i <= 2; i++){
+    console.log(getElement)
   }
 }
 
