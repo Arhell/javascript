733a4b6edabb149a5cf93cd5e0d16d836a491823
isychevskyi <arhell333@gmail.com>
2019-04-04 14:58:05 +0300

    find num length

---
+++ js-practice/js-array-training/5/app.js
@@ -0,0 +1,16 @@
+var arr = [11,21,11,19,11,17,11,15,11,13];
+//var count = 0;
+// for(i = 0; i < arr.length; ++i){
+//   if(arr[i] === 11)
+//     count++;
+// }
+
+var Arr = arr.filter(function(number) {
+  return number === 11;
+}).length;
+
+
+console.log(Arr)
+
+
+
