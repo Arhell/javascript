00d961a329029824dc82badef59b8fdc8c160fdd
isychevskyi <arhell333@gmail.com>
2019-04-08 18:02:01 +0300

    fix task

--- js-practice/js-array-training/5/app.js
+++ js-practice/js-array-training/5/app.js
@@ -1,16 +1,19 @@
 var arr = [11,21,11,19,11,17,11,15,11,13];
-//var count = 0;
-// for(i = 0; i < arr.length; ++i){
-//   if(arr[i] === 11)
-//     count++;
-// }
+var count = 0;
+for(i = 0; i < arr.length; ++i){
+  if(arr[i] === 11) {
+    count++;
+  }
+}
 
-var Arr = arr.filter(function(number) {
-  return number === 11;
-}).length;
+console.log(count)
+
+// var Arr = arr.filter(function(number) {
+//   return number === 11;
+// }).length;
 
 
-console.log(Arr)
+// console.log(Arr)
 
 
 
