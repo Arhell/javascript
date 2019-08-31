812ffb3b0ef91d1521bd153d246a16a4b0144565
Arhell <arhell333@gmail.com>
2019-08-31 22:01:47 +0300

    types

---
+++ js-basic/tutorial/3/script.js
@@ -0,0 +1,17 @@
+/*
+  Number
+  String
+  Boolean
+  Object
+  Null
+  Undefined
+ */
+
+var number = 3
+var string = 'Some text'
+var isTrue = true // false
+var obj = {a: 1}
+var nothing = null
+var und = undefined
+
+console.log(typeof number)
