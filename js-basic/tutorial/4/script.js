75a90618a9ef0a88f389da9a7a138a9016848275
Arhell <arhell333@gmail.com>
2019-09-01 01:54:23 +0300

    op

---
+++ js-basic/tutorial/4/script.js
@@ -0,0 +1,22 @@
+var num1 = 30
+var num2 = 3
+
+num1 = 2
+
+console.log('+:', num1 + num2)
+console.log('-:', num1 - num2)
+console.log('*:', num1 * num2)
+console.log('/:', num1 / num2)
+
+var str1 = 'some'
+var str2 = 'text'
+
+console.log('+:', str1 + ' ' + str2)
+
+console.log('1 + 2', 1 + '2')
+console.log('str1 + num1: ', str1 + num1)
+
+console.log('bool + string:', true + str2)
+
+console.log('true + 1', true + 1)
+console.log('false + 1', false + 1)
