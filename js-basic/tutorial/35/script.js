d8e280fd8867d7b4471f6cad06cd9f7a1476dcbd
Arhell <arhell333@gmail.com>
2019-10-20 22:28:39 +0300

    time & refactoring

--- js-basic/tutorial/35/script.js
+++ js-basic/tutorial/35/script.js
@@ -4,21 +4,30 @@ var $time = document.querySelector('#time')
 var $result = document.querySelector('#result')
 var $timeHeader = document.querySelector('#time-header')
 var $resultHeader = document.querySelector('#result-header')
+var $gameTime = document.querySelector('#game-time')
 
 var score = 0
 var isGameStarted = false
 
 $start.addEventListener('click', startGame)
 $game.addEventListener('click', handleBoxClick)
+$gameTime.addEventListener('input', setGmaseTime)
+
+function show($el) {
+  $el.classList.remove('hide')
+}
+
+function hide($el) {
+  $el.classList.add('hide')
+}
 
 function startGame() {
   score = 0
   setGmaseTime()
-  $timeHeader.classList.remove('hide')
-  $resultHeader.classList.add('hide')
+  $gameTime.setAttribute('disabled', true)
   isGameStarted = true
   $game.style.backgroundColor = '#fff'
-  $start.classList.add('hide')
+  hide($start)
 
   var interval = setInterval(function () {
     var time = parseFloat($time.textContent)
@@ -39,18 +48,21 @@ function setGameScore() {
 }
 
 function setGmaseTime() {
-  var time = 5
+  var time = +$gameTime.value
   $time.textContent = time.toFixed(1)
+  show($timeHeader)
+  hide($resultHeader)
 }
 
 function endGame() {
   isGameStarted = false
   setGameScore()
+  $gameTime.removeAttribute('disabled')
   $game.innerHTML = ''
-  $start.classList.remove('hide')
+  show($start)
   $start.style.backgroundColor = '#ccc'
-  $timeHeader.classList.add('hide')
-  $resultHeader.classList.remove('hide')
+  hide($timeHeader)
+  show($resultHeader)
 }
 
 function handleBoxClick(event) {
