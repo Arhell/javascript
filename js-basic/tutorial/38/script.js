3a766019fc6a1403a637d019e54f2d3d12e46284
Arhell <arhell333@gmail.com>
2019-11-12 00:15:09 +0200

    protoNext

---
+++ js-basic/tutorial/38/script.js
@@ -0,0 +1,12 @@
+var ford = Object.create({
+  calculateDistancePerYear: function () {
+    console.log('Calculate')
+  }
+}, {
+  name: {value: 'Ford'},
+  model: {value: 'Focus'},
+  year: {value: 2015},
+  distance: {value: 120500}
+})
+
+console.log(ford)
