50c96745da094b1dfc15ecc2f174e7a0c2bedd70
Arhell <arhell333@gmail.com>
2019-11-13 00:36:25 +0200

    func

--- js-basic/tutorial/38/script.js
+++ js-basic/tutorial/38/script.js
@@ -3,10 +3,41 @@ var ford = Object.create({
     console.log('Calculate')
   }
 }, {
-  name: {value: 'Ford'},
-  model: {value: 'Focus'},
-  year: {value: 2015},
-  distance: {value: 120500}
+  name: {
+    value: 'Ford',
+    enumerable: true,
+    writable: false,
+    configurable: false
+  },
+  model: {
+    value: 'Focus',
+    enumerable: true,
+    writable: false,
+    configurable: false
+
+  },
+  year: {
+    value: 2015,
+    enumerable: true,
+    writable: false,
+    configurable: false
+  },
+  distance: {
+    value: 120500,
+    enumerable: true,
+    writable: true,
+    configurable: false
+  },
+  age: {
+    enumerable: true,
+    get: function () {
+      console.log('Get age')
+      return new Date().getFullYear() - this.year
+    },
+    set: function () {
+      console.log('set value')
+    }
+  }
 })
 
 console.log(ford)
