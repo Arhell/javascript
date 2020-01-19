7db59f9785e593457f8ffca01469c2b22b3e43bd
Arhell <arhell333@gmail.com>
2020-01-20 01:34:49 +0200

    let const

---
+++ js/tutorial/44/script.js
@@ -0,0 +1,26 @@
+let color = 'red'
+
+color = {color: 'blue'}
+
+console.log(color)
+
+for (let i = 0; i < 5; i++) {
+  setTimeout(function () {
+    console.log(i)
+  }, 2000)
+}
+
+const HEX = '#ff0000'
+
+const arr = [1, 2]
+
+const onl = {a:1}
+
+arr.unshift(4)
+
+obj.b = 2
+
+console.log(arr)
+console.log(obj)
+
+document.querySelector('h1').style.color = HEX
