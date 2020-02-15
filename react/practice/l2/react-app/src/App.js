540e271a3bcd784546013bbf725f1b1bf56ce335
Arhell <arhell333@gmail.com>
2020-02-15 17:18:15 +0200

    content

--- react/practice/l2/react-app/src/App.js
+++ react/practice/l2/react-app/src/App.js
@@ -11,8 +11,12 @@ function App() {
     <div style={divStyle}>
       <h1>H1</h1>
 
-      <Car name={'Car'} year={2022}/>
-      <Car name={'Car2'} year={2023}/>
+      <Car name={'Car'} year={2022}>
+        <p style={{color: 'green'}}>color</p>
+      </Car>
+      <Car name={'Car2'} year={2023}>
+        <p style={{color: 'red'}}>color</p>
+      </Car>
       <Car name='Car3' year={2033}/>
     </div>
   );
