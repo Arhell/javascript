5e6c7473724249e3af2b82fd44f3ec8ce738992c
Arhell <arhell333@gmail.com>
2020-02-16 22:54:52 +0200

    state

--- react/practice/l2/react-app/src/App.js
+++ react/practice/l2/react-app/src/App.js
@@ -6,18 +6,34 @@ const divStyle = {
   textAlign: 'center'
 };
 
+const state = {
+  cars: [
+    {
+      name: 'Car',
+      year: 2022
+    },
+    {
+      name: 'Car2',
+      year: 2023
+    },
+    {
+      name: 'Car3',
+      year: 2033
+    },
+  ],
+  pageTitle: 'React components'
+}
+
 function App() {
+
   return (
     <div style={divStyle}>
-      <h1>H1</h1>
+      <h1>{state.pageTitle}</h1>
 
-      <Car name={'Car'} year={2022}>
-        <p style={{color: 'green'}}>color</p>
-      </Car>
-      <Car name={'Car2'} year={2023}>
-        <p style={{color: 'red'}}>color</p>
-      </Car>
-      <Car name='Car3' year={2033}/>
+      <Car name={state.cars[0].name} year={state.cars[0].year} />
+      <Car name={state.cars[1].name} year={state.cars[1].year} />
+      <Car name={state.cars[2].name} year={state.cars[2].year} />
+
     </div>
   );
 }
