43920299afa0672ca77502e32d0976e01fec092d
Arhell <arhell333@gmail.com>
2020-02-27 00:41:48 +0200

    github users add

--- js/github-users/modules/api.js
+++ js/github-users/modules/api.js
@@ -5,4 +5,15 @@ export class Api {
   async loadUsers(value, page) {
     return await fetch(`${URL}search/users?q=${value}&per_page=${USER_PER_PAGE}&page=${page}`)
   }
+
+  loadUserData(login) {
+    const urls = [
+      `${URL}users/${login}/following`,
+      `${URL}users/${login}/followers`,
+      `${URL}users/${login}/repos`,
+    ]
+    const requests = urls.map(urls => fetch(urls))
+    return Promise.all(requests)
+      .then(responses => Promise.all((responses.map(r => r.json()))))
+  }
 }
