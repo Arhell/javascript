b55b12c881034c45cac9b712ff9b094d1212dbdb
Arhell <arhell333@gmail.com>
2020-03-01 22:24:07 +0200

    dynamic lists

--- react/practice/l2/react-app/src/Car/Car.js
+++ react/practice/l2/react-app/src/Car/Car.js
@@ -1,28 +1,16 @@
 import React from "react";
 
-// function car() {
-//   return (
-//       <h2>This is cat component</h2>
-//     )
-// }
-
-// const car = () => {
-//   return (
-//       <div>This is cat component</div>
-//     )
-// }
-
-// const car = () => (
-//   <div>
-//     This is cat <strong>component</strong>
-//   </div>
-// )
-
 export default props => (
-  <div>
+  <div style={{
+    border: '1px solud #ccc',
+    marginBottom: '10px',
+    display: 'block',
+    padding: '10px'
+  }}>
     <h3>Car name: {props.name}</h3>
     <p>Year: <strong>{props.year}</strong></p>
-    <button onClick={props.onChangeTitle}>click</button>
+    <input type="text" onChange={props.onChangeName} value={props.name}/>
+    <button onClick={props.onDelete}>Delete</button>
   </div>
 )
 
