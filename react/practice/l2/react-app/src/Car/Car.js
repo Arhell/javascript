0e437398dc632cd0217c7b875a5b748e7da08eb6
Arhell <arhell333@gmail.com>
2020-03-06 00:14:49 +0200

    radium

--- react/practice/l2/react-app/src/Car/Car.js
+++ react/practice/l2/react-app/src/Car/Car.js
@@ -1,21 +1,32 @@
 import React from "react";
-import './Car.css'
+import Radium from "radium";
+import './Car.css';
 
-export default props => {
+const Car = props => {
   const inputCalsses = ['input']
 
-  if(props.name !== '') {
+  if (props.name !== '') {
     inputCalsses.push('green')
   } else {
     inputCalsses.push('red')
   }
 
-  if(props.name.length > 4) {
+  if (props.name.length > 4) {
     inputCalsses.push('bold')
   }
 
-  return(
-    <div className='Car'>
+  const style = {
+    border: '1px solid #ccc',
+    boxShadow: '0 4px 5px 0 rgba(0, 0, 0, .14)',
+    ':hover': {
+      border: '1px solid #aaa',
+      boxShadow: '0 4px 15px 0 rgba(0, 0, 0, .15)',
+      cursor: 'pointer'
+    }
+  }
+
+  return (
+    <div className='Car' style={style}>
       <h3>Car name: {props.name}</h3>
       <p>Year: <strong>{props.year}</strong></p>
       <input
@@ -23,10 +34,11 @@ export default props => {
         onChange={props.onChangeName}
         value={props.name}
         className={inputCalsses.join(' ')}
-        />
+      />
       <button onClick={props.onDelete}>Delete</button>
     </div>
   )
 }
 
+export default Radium(Car)
 
