dac230c71b68be494a1be463b2b86b40ac0f67fc
Arhell <arhell333@gmail.com>
2020-05-15 10:25:56 +0300

    dynamic routes

--- react/practice/l4/app-react/src/App.js
+++ react/practice/l4/app-react/src/App.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react'
 import './App.scss'
-import {Route, NavLink} from 'react-router-dom'
+import {Route, NavLink, Switch} from 'react-router-dom'
 import About from './About/About'
 import Cars from './Cars/Cars'
+import CarDetail from './CarDetail/CarDetail'
 
 class App extends Component {
   render() {
@@ -36,10 +37,13 @@ class App extends Component {
 
         <hr/>
 
-        <Route path="/" exact render={() => <h1>Home page</h1>} />
+        <Switch>
+          <Route path="/" exact render={() => <h1>Home page</h1>} />
+          <Route path="/about" component={About} />
+          <Route path="/cars/:name" component={CarDetail} />
+          <Route path="/cars" component={Cars} />
+        </Switch>
 
-        <Route path="/about" component={About} />
-        <Route path="/cars" component={Cars} />
 
       </div>
     );
