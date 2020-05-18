8fd576beb646a4a103c1e6097e1590321c8aba3b
Arhell <arhell333@gmail.com>
2020-05-19 00:04:40 +0300

    route protection

--- react/practice/l4/app-react/src/App.js
+++ react/practice/l4/app-react/src/App.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react'
 import './App.scss'
-import {Route, NavLink, Switch, Redirect} from 'react-router-dom'
+import {Route, NavLink, Switch,} from 'react-router-dom'
 import About from './About/About'
 import Cars from './Cars/Cars'
 import CarDetail from './CarDetail/CarDetail'
 
 class App extends Component {
+
+  state = {
+    isLoggedIn: false
+  }
+
   render() {
 
     return (
@@ -22,9 +27,6 @@ class App extends Component {
             <li>
               <NavLink
                 to="/about"
-                activeStyle={{
-                  color: green,
-                }}
               >About</NavLink>
             </li>
             <li>
@@ -37,9 +39,17 @@ class App extends Component {
 
         <hr/>
 
+        <div style={{textAlign: "center"}}>
+          <h3>Some text {this.state.isLoggedIn ? "true" : "false"}</h3>
+          <button onClick={() => this.setState({isLoggedIn: true})}>Login</button>
+        </div>
+
+        <hr/>
+
         <Switch>
           <Route path="/" exact render={() => <h1>Home page</h1>} />
-          <Route path="/about" component={About} />
+
+          { this.state.isLoggedIn ? <Route path="/about" component={About} /> : null}
           <Route path="/cars/:name" component={CarDetail} />
           <Route path="/cars" component={Cars} />
           {/*<Redirect to={'/'} />*/}
