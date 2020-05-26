5a62d11cd326bc81395094dcadc3172649272d1b
Arhell <arhell333@gmail.com>
2020-05-26 20:59:08 +0300

    navigation

--- react/practice/l3/react-com/src/components/Navigation/Drawer/Drawer.js
+++ react/practice/l3/react-com/src/components/Navigation/Drawer/Drawer.js
@@ -1,18 +1,31 @@
 import React, {Component} from "react";
 import Backdrop from '../../UI/Backdrop/Backdrop'
+import {NavLink} from 'react-router-dom'
 
-const links = [1, 2, 3]
+const links = [
+  {to: '/', label: 'List', exact: true},
+  {to: '/auth', label: 'Auth', exact: false},
+  {to: '/quiz-creator', label: 'Create test', exact: false},
+]
 
 class Drawer extends Component{
+
+  clickHandler = () => {
+    this.props.onClose()
+  }
 
   renderLinks() {
     return links.map((link, index) => {
       return (
         <li key={index}>
-          {/*eslint-disable-next-line*/}
-          <a>
-            Link {link}
-          </a>
+          <NavLink
+            to={link.to}
+            exact={link.exact}
+            activeClassName={'active'}
+            onClick={this.clickHandler}
+          >
+            {link.label}
+          </NavLink>
         </li>
       )
     })
