c84414e1db7bf806193ca211abb5d31938a158ac
Arhell <arhell333@gmail.com>
2020-05-27 00:40:07 +0300

    list page

--- react/practice/l3/react-com/src/containers/QuizList/QuizList.js
+++ react/practice/l3/react-com/src/containers/QuizList/QuizList.js
@@ -1,10 +1,32 @@
 import React, {Component} from "react";
+import {NavLink} from 'react-router-dom'
 
 export default class QuizList extends Component {
+
+  renderQuizes() {
+    return [1,2,3].map((quiz, index) => {
+      return (
+        <li
+          key={index}
+        >
+          <NavLink to={'/quiz/' + quiz}>
+            test {quiz}
+          </NavLink>
+        </li>
+      )
+    })
+  }
+
   render() {
     return (
-      <div>
-        <h1>QuizList</h1>
+      <div className='QuizList'>
+        <div>
+          <h1>Test list</h1>
+
+          <ul>
+            { this.renderQuizes() }
+          </ul>
+        </div>
       </div>
     )
   }
