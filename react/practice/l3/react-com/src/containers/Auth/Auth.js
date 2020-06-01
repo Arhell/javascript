d4cb1ab5b37debaafd5cbd426a28c4019dd01db5
Arhell <arhell333@gmail.com>
2020-06-01 21:49:21 +0300

    login page

--- react/practice/l3/react-com/src/containers/Auth/Auth.js
+++ react/practice/l3/react-com/src/containers/Auth/Auth.js
@@ -1,10 +1,45 @@
 import React, {Component} from "react";
+import Button from "../../components/UI/Button/Button"
 
 export default class Auth extends Component {
+
+  loginHandler = () => {
+
+  }
+
+  registerHandler = () => {
+
+  }
+
+  submitHandler = event => {
+    event.preventDefault()
+  }
+
   render() {
     return (
-      <div>
-        <h1>Auth</h1>
+      <div className="Auth">
+        <div>
+          <h1>Auth</h1>
+
+          <form onSubmit={this.submitHandler} className='AuthForm'>
+            <input type="text"/>
+            <input type="text"/>
+
+            <Button
+              type="success"
+              onClisk={this.loginHandler}
+            >
+              Login
+            </Button>
+            <Button
+              type="primary"
+              onClisk={this.registerHandler}
+            >
+              Registration
+            </Button>
+
+          </form>
+        </div>
       </div>
     )
   }
