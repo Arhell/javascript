ceaa4e5380fee7e25ac18d6c480218b53d81b1b5
Arhell <arhell333@gmail.com>
2020-06-03 20:41:31 +0300

    val

--- react/practice/l3/react-com/src/containers/Auth/Auth.js
+++ react/practice/l3/react-com/src/containers/Auth/Auth.js
@@ -4,6 +4,35 @@ import Input from "../../components/UI/Input/Input"
 
 export default class Auth extends Component {
 
+  state = {
+    formControls: {
+      email: {
+        value: '',
+        type: 'email',
+        label: 'Email',
+        errorMessage: 'Add correct email',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          email: true
+        }
+      },
+      password: {
+        value: '',
+        type: 'password',
+        label: 'Password',
+        errorMessage: 'Add correct password',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          minLength: 6
+        }
+      }
+    }
+  }
+
   loginHandler = () => {
 
   }
@@ -16,6 +45,29 @@ export default class Auth extends Component {
     event.preventDefault()
   }
 
+  onChangeHandler = (event, controlName) => {
+
+  }
+
+  renderInputs() {
+    return  Object.keys(this.state.formControls).map((controlName, index) => {
+      const control = this.state.formControls[controlName]
+      return (
+        <Input
+          key={controlName + index}
+          type={control.type}
+          value={control.value}
+          valid={control.valid}
+          touched={control.touched}
+          label={control.label}
+          shouldValidate={!!control.validation}
+          errorMessage={control.errorMessage}
+          onChange={event => this.onChangeHandler(event, controlName)}
+        />
+      )
+    })
+  }
+
   render() {
     return (
       <div className="Auth">
@@ -24,14 +76,7 @@ export default class Auth extends Component {
 
           <form onSubmit={this.submitHandler} className='AuthForm'>
 
-            <Input
-              label="Email"
-            />
-
-            <Input
-              label="Password"
-              errorMessage={'test'}
-            />
+            { this.renderInputs() }
 
             <Button
               type="success"
