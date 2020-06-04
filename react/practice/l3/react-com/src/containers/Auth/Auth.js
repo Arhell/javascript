658672e6f49b343389e4bb3d2b6f1cf16f2055b6
Arhell <arhell333@gmail.com>
2020-06-04 21:57:11 +0300

    val2

--- react/practice/l3/react-com/src/containers/Auth/Auth.js
+++ react/practice/l3/react-com/src/containers/Auth/Auth.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import Button from "../../components/UI/Button/Button"
 import Input from "../../components/UI/Input/Input"
+import is from 'is-js'
 
 export default class Auth extends Component {
 
@@ -45,8 +46,41 @@ export default class Auth extends Component {
     event.preventDefault()
   }
 
-  onChangeHandler = (event, controlName) => {
+  validateControl(value, validation) {
+    if (!validation) {
+      return true
+    }
 
+    let isValid = true
+
+    if(validation.required) {
+      isValid = value.trim() !== '' && isValid
+    }
+
+    if(validation.email) {
+      isValid = is.email(value) && isValid
+    }
+
+    if(validation.minLength) {
+      isValid = value.length >= validation.minLength && isValid
+    }
+
+    return isValid
+  }
+
+  onChangeHandler = (event, controlName) => {
+    const formControls = {...this.state.formControls}
+    const control = { ...formControls[controlName] }
+
+    control.value = event.target.value
+    control.touched = true
+    control.valid = this.validateControl(control.value, control.validation)
+
+    formControls[controlName] = control
+
+    this.setState({
+      formControls
+    })
   }
 
   renderInputs() {
