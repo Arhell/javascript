3f791dd76ff5ae9f75ae1a01fd9470b0ecfb1f1b
Arhell <arhell333@gmail.com>
2020-06-09 17:49:49 +0300

    create test

--- react/practice/l3/react-com/src/containers/QuizCreator/QuizCreator.js
+++ react/practice/l3/react-com/src/containers/QuizCreator/QuizCreator.js
@@ -1,10 +1,52 @@
 import React, {Component} from "react";
+import Button from '../../components/UI/Button/Button'
 
 export default class QuizCreator extends Component {
+
+  submitHandler = (event) => {
+    event.preventDefault()
+  }
+
+  addQuestionHandler = () => {
+
+  }
+
+  createQuizHandler = () => {
+
+  }
+
   render() {
     return (
-      <div>
-        <h1>QuizCreator</h1>
+      <div className="QuizCreator">
+        <div>
+          <h1>Create test</h1>
+
+          <form onSubmit={this.submitHandler}>
+
+            <input type="text"/>
+            <hr />
+            <input type="text"/>
+            <input type="text"/>
+            <input type="text"/>
+            <input type="text"/>
+
+            <select></select>
+
+            <Button
+              type="primary"
+              onClick={this.addQuestionHandler}
+            >
+              Add question
+            </Button>
+
+            <Button
+              type="success"
+              onClick={this.createQuizHandler}
+            >
+              Add test
+            </Button>
+          </form>
+        </div>
       </div>
     )
   }
