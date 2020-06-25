e0ed9db58d269abd1c878bebb65c2c95359bda64
Arhell <arhell333@gmail.com>
2020-06-25 18:08:38 +0300

    login

--- react/practice/l3/react-com/src/containers/Auth/Auth.js
+++ react/practice/l3/react-com/src/containers/Auth/Auth.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import Button from "../../components/UI/Button/Button"
 import Input from "../../components/UI/Input/Input"
 import is from 'is-js'
+import axios from 'axios'
 
 export default class Auth extends Component {
 
@@ -34,12 +35,32 @@ export default class Auth extends Component {
     }
   }
 
-  loginHandler = () => {
-
+  loginHandler = async () => {
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true
+    }
+    try {
+      const response = await axios.post('', authData)
+      console.markTimeline(response.data)
+    } catch (e) {
+      console.log(e)
+    }
   }
 
-  registerHandler = () => {
-
+  registerHandler = async () => {
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true
+    }
+    try {
+      const response = await axios.post('', authData)
+      console.markTimeline(response.data)
+    } catch (e) {
+      console.log(e)
+    }
   }
 
   submitHandler = event => {
