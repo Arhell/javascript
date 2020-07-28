718214c4c826cf1267778d4a6df03682a88f6594
Arhell <arhell333@gmail.com>
2020-07-28 14:59:10 +0300

    menu list

--- react/practice/l3/react-com/src/App.js
+++ react/practice/l3/react-com/src/App.js
@@ -1,22 +1,57 @@
 import React from 'react';
 import Layout from "./hoc/Layout/Layout";
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, Redirect, wishRouter} from 'react-router-dom';
 import Quiz from "./containers/Quiz/Quiz";
 import QuizList from "./containers/QuizList/QuizList";
 import Auth from "./containers/Auth/Auth";
 import QuizCreator from "./containers/QuizCreator/QuizCreator";
+import {connect} from 'react-redux'
+import Logout from "./components/Logout/Logout";
 
 function App() {
-  return (
-    <Layout>
+
+  componentDidMount() {
+    this.props.autoLogin()
+  }
+
+  let routs = (
+    <Switch>
+      <Route path={'/auth'} component={Auth} />
+      <Route path={'/quiz/:id'} component={Quiz} />
+      <Route path={'/'} component={QuizList} />
+      <Redirect to={'/'} />
+    </Switch>
+  )
+
+  if(this.props.isAuthenticated) {
+    routs = (
       <Switch>
-        <Route path={'/auth'} component={Auth} />
         <Route path={'/quiz-creator'} component={QuizCreator} />
         <Route path={'/quiz/:id'} component={Quiz} />
         <Route path={'/'} component={QuizList} />
+        <Route path={'/logout'} component={Logout} />
+        <Redirect to={'/'} />
       </Switch>
+    )
+  }
+
+  return (
+    <Layout>
+      {routs}
     </Layout>
   );
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    isAuthenticated: !!state.auth.token
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    autoLogin: () => dispatch(autoLogin())
+  }
+}
+
+export default wishRouter(connect(mapStateToProps)(App))
--- react/practice/l3/react-com/src/components/Navigation/Drawer/Drawer.js
+++ react/practice/l3/react-com/src/components/Navigation/Drawer/Drawer.js
@@ -2,19 +2,13 @@ import React, {Component} from "react";
 import Backdrop from '../../UI/Backdrop/Backdrop'
 import {NavLink} from 'react-router-dom'
 
-const links = [
-  {to: '/', label: 'List', exact: true},
-  {to: '/auth', label: 'Auth', exact: false},
-  {to: '/quiz-creator', label: 'Create test', exact: false},
-]
-
 class Drawer extends Component{
 
   clickHandler = () => {
     this.props.onClose()
   }
 
-  renderLinks() {
+  renderLinks(links) {
     return links.map((link, index) => {
       return (
         <li key={index}>
@@ -39,11 +33,22 @@ class Drawer extends Component{
       cls.push('close')
     }
 
+    const links = [
+      {to: '/', label: 'List', exact: true},
+    ]
+
+    if(this.props.isAuthenticated) {
+      links.push({to: '/quiz-creator', label: 'Create test', exact: false})
+      links.push({to: '/logout', label: 'Logout', exact: false})
+    } else {
+      links.push({to: '/auth', label: 'Auth', exact: false})
+    }
+
     return(
       <React.Fragment>
         <nav className={cls.join(' ')}>
           <ul>
-            {this.renderLinks()}
+            {this.renderLinks(links)}
           </ul>
         </nav>
         { this.props.isOpen ? <Backdrop onClick={this.props.onClose} /> : null }
