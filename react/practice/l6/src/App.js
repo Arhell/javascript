e79ae66a2398fcbbf44f89fff8837161cb005846
Arhell <arhell333@gmail.com>
2020-08-03 21:58:00 +0300

    app

--- react/practice/l6/src/App.js
+++ react/practice/l6/src/App.js
@@ -2,9 +2,29 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  state = {
+    chars: [
+      {name: '1', side: '1'},
+      {name: '2', side: '2'},
+      {name: '3', side: '1'},
+      {name: '4', side: '2'}
+    ]
+  }
+
   return (
-    <div>
-    </div>
+    <ul>
+      { this.state.chars.map((char, index) => {
+        return (
+          <li> key={char.name + index}
+            <strong>
+              {char.name} - &nbsp;
+            </strong>
+
+            {char.side}
+          </li>
+        )
+      })}
+    </ul>
   );
 }
 
