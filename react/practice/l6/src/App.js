6094d62e1d3b0fb2fa27e898383abe208a100404
Arhell <arhell333@gmail.com>
2020-08-04 21:24:05 +0300

    optimization

--- react/practice/l6/src/App.js
+++ react/practice/l6/src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import './App.css';
 
+const chars = [
+  {name: '1', side: '1'},
+  {name: '2', side: '2'},
+  {name: '3', side: '1'},
+  {name: '4', side: '2'}
+]
+
 function App() {
-  state = {
-    chars: [
-      {name: '1', side: '1'},
-      {name: '2', side: '2'},
-      {name: '3', side: '1'},
-      {name: '4', side: '2'}
-    ]
-  }
 
   return (
     <ul>
-      { this.state.chars.map((char, index) => {
+      { chars.map((char, index) => {
         return (
           <li> key={char.name + index}
             <strong>
