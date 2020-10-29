843aceadda407fcd3263aadca0a635c1d06e2789
Arhell <arhell333@gmail.com>
2020-10-29 19:40:55 +0200

    map 2

--- js/tutorial/76/index.js
+++ js/tutorial/76/index.js
@@ -30,3 +30,33 @@ console.log(map.has('job'))
 console.log(map.size)
 map.clear()
 console.log(map.size)
+
+for (let entry of map.entries()) {
+  console.log(entry)
+}
+
+for (let [key, value] of map.entries()) {
+  console.log(key, value)
+}
+
+for (let val of map.values()) {
+  console.log(val)
+}
+
+for (let key of map.keys()) {
+  console.log(key)
+}
+
+map.forEach((val, key, map) => {
+  console.log(val, key)
+})
+
+const array = [...map]
+console.log(array)
+
+const array2 = Array.from(map)
+
+console.log(array2)
+
+const mapObj = Object.fromEntries(map.entries())
+console.log(mapObj)
