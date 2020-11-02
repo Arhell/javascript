dca7da4361bbc6bb37846369c84b3e784a1140a9
Arhell <arhell333@gmail.com>
2020-11-02 20:55:13 +0200

    set

--- js/tutorial/76/index.js
+++ js/tutorial/76/index.js
@@ -78,3 +78,26 @@ function lastVisit(user) {
 }
 
 console.log(lastVisit(users[1]))
+
+
+const set = new Set([1,2,3,3,3,4,5,5,6])
+set.add(10).add(20).add(30).add(20)
+console.log(set)
+console.log(set.has(30))
+console.log(set.size)
+console.log(set.delete(1))
+console.log(set.values())
+console.log(set.keys())
+console.log(set.entries())
+
+for(let key of set) {
+  console.log(key)
+}
+
+function uniqValues(array) {
+  return [...new Set(array)]
+  // return Array.from(new Set(array))
+}
+
+console.log(uniqValues([1,1,2,2,4,4,4,4,4,5,6,6,6]))
+
