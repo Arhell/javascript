a429d4a85496c9004f4f7fca94db54546776f4e4
Arhell <arhell333@gmail.com>
2020-11-19 19:02:10 +0200

    useCallback next

--- react/hooks/src/App.js
+++ react/hooks/src/App.js
@@ -1,4 +1,5 @@
-import {useState, useMemo,} from 'react'
+import {useState, useCallback,} from 'react'
+import ItemsList from './itemsList'
 import './App.css';
 
 function App() {
@@ -10,11 +11,18 @@ function App() {
     color: colored ? 'red' : 'black'
   }
 
+  const generateItemsFromApi = useCallback(() => {
+    return new Array(count).fill('').map(i => `Elem ${i + 1}`)
+  }, [count])
+
   return (
     <div>
       <h1 style={styles}>{count}</h1>
       <button onClick={() => setCount(prev => prev + 1)}>add</button>
       <button onClick={() => setColored(prev => !prev)}>change</button>
+
+
+      <ItemsList getItems={generateItemsFromApi} />
     </div>
   );
 }
