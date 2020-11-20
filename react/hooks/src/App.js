2dc3d099ce8f44359e26d871fc82544f37195a1f
Arhell <arhell333@gmail.com>
2020-11-20 12:58:14 +0200

    useContext

--- react/hooks/src/App.js
+++ react/hooks/src/App.js
@@ -1,28 +1,12 @@
-import {useState, useCallback,} from 'react'
-import ItemsList from './itemsList'
+import {useState} from 'react'
 import './App.css';
 
 function App() {
 
-  const [colored, setColored] = useState(false)
-  const [count, setCount] = useState(1)
-
-  const styles = {
-    color: colored ? 'red' : 'black'
-  }
-
-  const generateItemsFromApi = useCallback(() => {
-    return new Array(count).fill('').map(i => `Elem ${i + 1}`)
-  }, [count])
 
   return (
     <div>
-      <h1 style={styles}>{count}</h1>
-      <button onClick={() => setCount(prev => prev + 1)}>add</button>
-      <button onClick={() => setColored(prev => !prev)}>change</button>
-
-
-      <ItemsList getItems={generateItemsFromApi} />
+      
     </div>
   );
 }
