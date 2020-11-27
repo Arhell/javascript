7192c474c61aa8466c6851a23a698602d4c49d2a
Arhell <arhell333@gmail.com>
2020-11-27 12:00:14 +0200

    custom next

--- react/hooks/src/App.js
+++ react/hooks/src/App.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './App.css';
+
+function useLogger(value) {
+  useEffect(() => {
+    console.log('Val', value)
+  }, [value])
+}
+
+function useInput(initialValue) {
+  const [value, setValue] = useState(initialValue)
+
+  const onChange = event => {
+    setValue(event.target.value)
+  }
+
+  const clear = () => setValue('')
+
+  return {
+    bind: {value, onChange},
+    value,
+    clear
+  }
+}
 
 function App() {
 
+  // const [name, setName] = useState('')
+  //
+  // const changeHandler = event => {
+  //   setName(event.target.value)
+  // }
+
+  const input = useInput('')
+
+  useLogger(input.value)
+
   return (
     <div>
-
+      <input type="text" {...input.bind} />
+      <button onClick={() => input.clear()}>Clear</button>
+      <hr />
+      <h1>{input.value}</h1>
     </div>
   );
 }
