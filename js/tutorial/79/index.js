059e8605f3edf60c97141007dcef0e6fbac00775
Arhell <arhell333@gmail.com>
2020-12-07 18:51:39 +0200

    destructuring

---
+++ js/tutorial/79/index.js
@@ -0,0 +1,16 @@
+function calcValues(a, b) {
+  return [
+    a + b,
+    a - b,
+    a * b,
+    a / b
+  ]
+}
+
+// const result = calcValues(42, 10)
+const [sum,, mult, ...other] = calcValues(42, 10)
+// const sum = result[0]
+// const sub = result[1]
+// const [sum, sub] = result
+
+console.log(sum, mult, other)
