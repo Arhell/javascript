f04e6b054f48c124229093d8d111af7d00ecaa95
Arhell <arhell333@gmail.com>
2020-12-08 02:08:13 +0200

    destructuring next

--- js/tutorial/79/index.js
+++ js/tutorial/79/index.js
@@ -8,9 +8,36 @@ function calcValues(a, b) {
 }
 
 // const result = calcValues(42, 10)
-const [sum,, mult, ...other] = calcValues(42, 10)
+const [sum, sub = 'None', mult, ...other] = calcValues(42, 10)
 // const sum = result[0]
 // const sub = result[1]
 // const [sum, sub] = result
 
-console.log(sum, mult, other)
+console.log(sum, sub, mult, other)
+
+const person = {
+  name: 'Name',
+  age: 30,
+  adress: {
+    country: 'Country',
+    city: 'City'
+  }
+}
+
+// const name = person.name
+// const {
+//   name: firstName = "No name",
+//   age,
+//   car = 'None',
+//   adress: {city: homeTown, country}
+// } = person
+// console.log(firstName, age, car, homeTown, country)
+
+// const {name, ...info} = person
+// console.log(name, info)
+
+function logPerson({name: firstName = 'Name', age}) {
+  console.log(firstName + " " + age)
+}
+
+console.log(logPerson(person))
