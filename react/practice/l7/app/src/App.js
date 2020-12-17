26661d0688df18ff0242b6427d875aa5a2ef17e5
Arhell <arhell333@gmail.com>
2020-12-17 18:12:06 +0200

    add nav

--- react/practice/l7/app/src/App.js
+++ react/practice/l7/app/src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import {Home} from './pages/Home'
 import {About} from './pages/About'
+import {Navbar} from "./components/Navbar";
 
 function App() {
   return (
     <Router>
+        <Navbar />
         <div className="container pt-4">
           <Switch>
             <Route path={'/'} exact component={Home}/>
