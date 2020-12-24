d4adcee51f3688ef343648cba80bc33c19540cae
Arhell <arhell333@gmail.com>
2020-12-25 01:03:52 +0200

    form next

--- react/practice/l7/app/src/components/form.js
+++ react/practice/l7/app/src/components/form.js
@@ -8,7 +8,13 @@ export const Form = () => {
   const submitHandler = event => {
     event.preventDefault()
 
-    alert.show(value, 'success')
+    if(value.trim()) {
+      alert.show("Create", 'success')
+      setValue('')
+    } else {
+      alert.show('Add name')
+    }
+
   }
 
   return (
