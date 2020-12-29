eef03b9107a858395a2c3dd0e1dbf23bf3fd9b61
Arhell <arhell333@gmail.com>
2020-12-29 18:06:42 +0200

    state

--- react/practice/l7/app/src/context/firebase/FirebaseState.js
+++ react/practice/l7/app/src/context/firebase/FirebaseState.js
@@ -1,6 +1,10 @@
 import {useReducer} from 'react'
+import axios from "axios";
 import {FirebaseContext} from './firebaseContext'
 import {firebaseReducer} from "./firebaseReducer";
+import {REMOVE_NOTE, SHOW_LOADER} from "../types";
+
+const url = process.env.REACT_APP_DB_URL
 
 export const FirebaseState = ({children}) => {
 	const initialState = {
@@ -10,11 +14,38 @@ export const FirebaseState = ({children}) => {
 
 	const [state, dispatch] = useReducer(firebaseReducer, initialState)
 
+	const showLoader = () => dispatch({type: SHOW_LOADER})
 
+	const fetchNotes = async () => {
+		showLoader()
+		const res = await axios.get(`${url}/notes.json`)
+		console.log('fetch', res.data)
+	}
+
+	const addNote = async title => {
+		const note = {
+			title, data: new Date().toJSON()
+		}
+
+		const res = await axios.post(`${url}/notes.json`, note)
+
+		console.log('addNote', res.data)
+	}
+
+	const removeNote = async id => {
+		await axios.delete(`${url}/notes/${id}.json`)
+
+		dispatch({
+			type: REMOVE_NOTE,
+			payload: id
+		})
+	}
 
 	return (
 		<FirebaseContext.Provider value={{
-
+			showLoader, addNote, removeNote, fetchNotes,
+			loading: state.loading,
+			notes: state.notes
 		}}>
 			{children}
 		</FirebaseContext.Provider>
