99696cb7b7f673e4771e03e736f673b5a9a236fb
Arhell <arhell333@gmail.com>
2021-01-14 20:46:58 +0200

    navigation

--- next/app/pages/index.js
+++ next/app/pages/index.js
@@ -1,3 +1,18 @@
+import Link from 'next/link'
+
 export default function Index() {
-	return <h1>Text</h1>
+	return <>
+		<h1>Text</h1>
+		<p>
+			<Link href={'/about'}>
+				<a>About</a>
+			</Link>
+		</p>
+
+		<p>
+			<Link href={'/posts'}>
+				<a>Posts</a>
+			</Link>
+		</p>
+	</>
 }
--- next/app/pages/post/about/index.js
+++ next/app/pages/post/about/index.js
@@ -1,3 +1,18 @@
+import React from 'react'
+import Router from 'next/router'
+
 export default function About() {
-	return <h1>About</h1>
+
+	const linkClickHandler = () => {
+		Router.push('/')
+	}
+
+	return (
+		<React.Fragment>
+			<h1>About</h1>
+
+			<button onClick={linkClickHandler}>go back to home</button>
+			<button onClick={() => Router.push('/posts')}>go to posts</button>
+		</React.Fragment>
+	)
 }
