3174f634f6dd275509d35dbf0f8d1bd25806147e
Arhell <arhell333@gmail.com>
2021-01-26 01:02:26 +0200

    upd posts next

--- next/app/pages/posts.js
+++ next/app/pages/posts.js
@@ -1,18 +1,28 @@
 import {useState, useEffect} from 'react'
 import {MainLayout} from "../components/MainLayout";
 
-export default function Posts({posts}) {
-	// const [posts, setPosts] = useState([])
-	//
-	// useEffect(() => {
-	// 	async function load() {
-	// 		const response = await fetch('http://localhost:4200/posts')
-	// 		const json = await response.json()
-	// 		setPosts(json)
-	// 	}
-	//
-	// 	load()
-	// }, [])
+export default function Posts({posts: serverPosts}) {
+	const [posts, setPosts] = useState(serverPosts)
+
+	useEffect(() => {
+		async function load() {
+			const response = await fetch('http://localhost:4200/posts')
+			const json = await response.json()
+			setPosts(json)
+		}
+
+		if(!serverPosts) {
+			load()
+		}
+	}, [])
+
+	if(!posts) {
+		return (
+			<MainLayout>
+				<p>loading...</p>
+			</MainLayout>
+		)
+	}
 
 	return (
 		<MainLayout title={'Posts'}>
@@ -32,7 +42,10 @@ export default function Posts({posts}) {
 	)
 }
 
-Posts.getInitialProps = async () => {
+Posts.getInitialProps = async ({req}) => {
+	if(!req) {
+		return {posts: null}
+	}
 	const response = await fetch('http://localhost:4200/posts')
 	const posts = await response.json()
 
