d2853f06479a45713fdcdbf37bf39d3873b8c88b
Arhell <arhell333@gmail.com>
2021-01-25 09:02:05 +0200

    upd posts

--- next/app/pages/posts.js
+++ next/app/pages/posts.js
@@ -1,25 +1,42 @@
 import {useState, useEffect} from 'react'
 import {MainLayout} from "../components/MainLayout";
 
-export default function Posts() {
-	const [posts, setPosts] = useState([])
-
-	useEffect(() => {
-		async function load() {
-			const response = await fetch('http://localhost:4200/posts')
-			const json = await response.json()
-			setPosts(json)
-		}
-
-		load()
-	}, [])
+export default function Posts({posts}) {
+	// const [posts, setPosts] = useState([])
+	//
+	// useEffect(() => {
+	// 	async function load() {
+	// 		const response = await fetch('http://localhost:4200/posts')
+	// 		const json = await response.json()
+	// 		setPosts(json)
+	// 	}
+	//
+	// 	load()
+	// }, [])
 
 	return (
 		<MainLayout title={'Posts'}>
 			<h1>Posts</h1>
-			<pre>
-				{JSON.stringify(posts, null, 2)}
-			</pre>
+			<ul>
+				{posts.map(post => (
+					<li key={post.id}>
+						<Link href={`/posts/[id]`} as={`/post/${post.id}`}>
+							<a>
+								{post.title}
+							</a>
+						</Link>
+					</li>
+				))}
+			</ul>
 		</MainLayout>
 	)
+}
+
+Posts.getInitialProps = async () => {
+	const response = await fetch('http://localhost:4200/posts')
+	const posts = await response.json()
+
+	return {
+		posts
+	}
 }
