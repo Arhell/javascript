0a3cde644892677b8f06302cb831eff4bb604dc4
Arhell <arhell333@gmail.com>
2021-02-07 01:16:38 +0200

    upd controller

--- nest/nest-app/src/products/products.controller.ts
+++ nest/nest-app/src/products/products.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Delete, Put } from '@nestjs/common';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  @Get()
+  getAll(): string {
+    return 'getAll';
+  }
+  @Get(':id')
+  getOne(@Param('id') id: string): string {
+    return 'getOne ' + id;
+  }
+  @Post()
+  create() {
+
+  }
+  @Delete()
+  remove() {
+
+  }
+  @Put()
+  update() {
+
+  }
+}
