5ef5f0eb07295b27704d42952c6e045b0f49dd0d
Arhell <arhell333@gmail.com>
2021-02-10 01:01:50 +0200

    upd

--- nest/nest-app/src/products/products.controller.ts
+++ nest/nest-app/src/products/products.controller.ts
@@ -1,11 +1,27 @@
-import { Controller, Get, Param, Post, Delete, Put, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Delete,
+  Put,
+  Body,
+  HttpCode,
+  Header,
+  HttpStatus,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Response, Request } from 'express';
 
 @Controller('products')
 export class ProductsController {
   @Get()
-  getAll(): string {
+  // @Redirect('https://some.com', 301)
+  getAll(@Req() req: Request, @Res() res: Response): string {
+    res.status(201).end('Text');
     return 'getAll';
   }
   @Get(':id')
@@ -13,6 +29,8 @@ export class ProductsController {
     return 'getOne ' + id;
   }
   @Post()
+  @HttpCode(HttpStatus.CREATED)
+  @Header('Cache-Control', 'none')
   create(@Body() createProductDto: CreateProductDto): string {
     return `Title: ${createProductDto.title} Price: ${createProductDto.price}`;
   }
