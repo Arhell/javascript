ead5bbf5d9f92a86c3f0b9b06443d4c59db11d33
Arhell <arhell333@gmail.com>
2021-02-11 00:38:25 +0200

    services

--- nest/nest-app/src/products/products.controller.ts
+++ nest/nest-app/src/products/products.controller.ts
@@ -15,29 +15,40 @@ import {
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Response, Request } from 'express';
+import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
-  @Get()
+  constructor(private readonly productsService: ProductsService) {}
+
+  // @Get()
   // @Redirect('https://some.com', 301)
-  getAll(@Req() req: Request, @Res() res: Response): string {
-    res.status(201).end('Text');
-    return 'getAll';
+  // getAll(@Req() req: Request, @Res() res: Response): string {
+  //   res.status(201).end('Text');
+  //   return 'getAll';
+  // }
+  @Get()
+  getAll() {
+    return this.productsService.getAll();
   }
+
   @Get(':id')
-  getOne(@Param('id') id: string): string {
+  getOne(@Param('id') id: string) {
     return 'getOne ' + id;
   }
+
   @Post()
   @HttpCode(HttpStatus.CREATED)
   @Header('Cache-Control', 'none')
-  create(@Body() createProductDto: CreateProductDto): string {
-    return `Title: ${createProductDto.title} Price: ${createProductDto.price}`;
+  create(@Body() createProductDto: CreateProductDto) {
+    return this.productsService.create(createProductDto);
   }
+
   @Delete(':id')
   remove(@Param('id') id: string) {
     return 'Remove' + id;
   }
+
   @Put(':id')
   update(@Body() updateProductDto: UpdateProductDto, @Param('id') id: string) {
     return 'Update ' + id;
