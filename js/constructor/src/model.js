e2911aec5b070ea0915f57b27c57a5196b39f935
Arhell <arhell333@gmail.com>
2021-02-28 19:29:58 +0200

    add class

--- js/constructor/src/model.js
+++ js/constructor/src/model.js
@@ -1,56 +1,46 @@
 import image from "./assets/img.jpg"
+import {Block} from './classes/blocks'
 
 export const model = [
-  {
-    type: 'title',
-    value: 'Text',
-    options: {
-      tag: 'h2',
-      styles: {
-        background: 'red',
-        color: '#fff',
-        padding: '1rem',
-        'text-align': 'center'
-      }
+  new Block('title', 'Text',{
+    tag: 'h2',
+    styles: {
+      background: 'red',
+      color: '#fff',
+      padding: '1rem',
+      'text-align': 'center'
     }
-  },
-  {
-    type: 'text',
-    value: 'Text2',
-    options: {
-      styles: {
-        padding: '1rem'
-      }
+  }),
+
+  new Block('title','Text', {
+    styles: {
+      padding: '1rem'
     }
-  },
-  {
-    type: 'columns',
-    value: [
-      '123',
-      '456',
-      '789'
+  }),
+
+  new Block('columns',
+    [
+          '123',
+          '456',
+          '789'
     ],
-    options: {
-      styles: {
-        padding: '2rem',
-        color: 'red'
-      }
-    }
-  },
-  {
-    type: 'image',
-    value: image,
-    options: {
-      styles: {
-        padding: '2rem 0',
-        display: 'flex',
-        'justify-content': 'center'
-      },
-      imageStyles: {
-        width: '500px',
-        height: 'auto'
-      },
-      alt: 'alt'
-    }
-  }
+{
+        styles: {
+          padding: '2rem',
+          color: 'red'
+        }
+    }),
+
+  new Block('image', image, {
+    styles: {
+      padding: '2rem 0',
+      display: 'flex',
+      'justify-content': 'center'
+    },
+    imageStyles: {
+      width: '500px',
+      height: 'auto'
+    },
+    alt: 'alt'
+  })
 ]
