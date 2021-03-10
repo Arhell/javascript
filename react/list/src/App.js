50b05ee9deac66aefd94303c0dfa0b49fac76203
Arhell <arhell333@gmail.com>
2021-03-11 00:18:55 +0200

    add new component

--- react/list/src/App.js
+++ react/list/src/App.js
@@ -1,4 +1,5 @@
 import List from './components/List';
+import AddListButton from './components/AddButtonList'
 
 function App() {
   return (
@@ -33,20 +34,7 @@ function App() {
           isRemovable
         />
 
-        <List
-          items={[
-            {
-              className: "list__add-button",
-              icon: (
-                <svg width="12" height="12" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M8 1V15" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                  <path d="M1 8H15" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                </svg>
-              ),
-              name: 'Добавить список'
-            }
-          ]}
-        />
+        <AddListButton />
       </div>
       <div className="todo__tasks">
         123
