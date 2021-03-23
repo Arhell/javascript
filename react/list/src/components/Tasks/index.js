12409f531a39818a76c873c2f34019fdc8d26f1a
Arhell <arhell333@gmail.com>
2021-03-24 01:04:01 +0200

    update component

--- react/list/src/components/Tasks/index.js
+++ react/list/src/components/Tasks/index.js
@@ -1,3 +1,5 @@
+import EditIcon from '../../assets/img/edit.svg'
+
 import './tasks.scss'
 
 const Tasks = () => {
@@ -6,7 +8,21 @@ const Tasks = () => {
       <div className="tasks">
         <h2 className="tasks-title">
           Text
+          <img src={EditIcon} alt="edit icon"/>
         </h2>
+        <div className="tasks__items">
+         <div className="tasks__items-row">
+           <div className="checkbox">
+             <input id="check" type="checkbox"/>
+             <label htmlFor="check">
+               <svg width="11" height="8" viewBox="0 0 11 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                 <path d="M9.29999 1.20001L3.79999 6.70001L1.29999 4.20001" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+               </svg>
+             </label>
+           </div>
+           <input value="Some text"/>
+         </div>
+        </div>
       </div>
     </div>
   )
