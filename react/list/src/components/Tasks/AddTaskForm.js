a8c74d97785ae8d82f0812f21786ed14036447be
Arhell <arhell333@gmail.com>
2021-04-06 18:40:55 +0300

    add form

--- react/list/src/components/Tasks/AddTaskForm.js
+++ react/list/src/components/Tasks/AddTaskForm.js
@@ -8,6 +8,15 @@ const AddTaskForm = () => {
         <img src={addIcon} alt="icon"/>
         <span>New task</span>
       </div>
+      <div className="tasks__form-block">
+        <input
+          className="field"
+          type="text"
+          placeholder="Текст задачи"
+        />
+        <button className="button">Добавить задачу</button>
+        <button className="button button--grey">Отмена</button>
+      </div>
     </div>
   )
 }
