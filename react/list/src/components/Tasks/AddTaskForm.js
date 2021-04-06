7eabdb62b7b2d374603ccf2f699bee942b68aad8
Arhell <arhell333@gmail.com>
2021-04-07 00:37:56 +0300

    upd form

--- react/list/src/components/Tasks/AddTaskForm.js
+++ react/list/src/components/Tasks/AddTaskForm.js
@@ -1,22 +1,37 @@
+import {useState} from 'react'
+
 import addIcon from '../../assets/img/add.svg'
 
 const AddTaskForm = () => {
+  const [visibleForm, setVisibleForm] = useState(false)
+
+  const toggleFormVisible = () => {
+    setVisibleForm(!visibleForm)
+  }
 
   return (
     <div className="tasks__form">
-      <div className="tasks__form-new">
-        <img src={addIcon} alt="icon"/>
-        <span>New task</span>
-      </div>
-      <div className="tasks__form-block">
-        <input
-          className="field"
-          type="text"
-          placeholder="Текст задачи"
-        />
-        <button className="button">Добавить задачу</button>
-        <button className="button button--grey">Отмена</button>
-      </div>
+
+      {!visibleForm
+        ? (
+          <div className="tasks__form-new" onClick={toggleFormVisible}>
+            <img src={addIcon} alt="icon"/>
+            <span>New task</span>
+          </div>
+        )
+
+        : (
+          <div className="tasks__form-block">
+            <input
+              className="field"
+              type="text"
+              placeholder="Текст задачи"
+            />
+            <button className="button">Добавить задачу</button>
+            <button className="button button--grey" onClick={toggleFormVisible}>Отмена</button>
+          </div>
+        )
+      }
     </div>
   )
 }
