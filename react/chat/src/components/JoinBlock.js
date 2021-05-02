fad6c20bb10185d7d6fafbed05060de75c01b756
Arhell <arhell333@gmail.com>
2021-05-02 12:42:17 +0300

    add reducer

--- react/chat/src/components/JoinBlock.js
+++ react/chat/src/components/JoinBlock.js
@@ -2,25 +2,30 @@ import { useState } from 'react'
 import socket from '../socket'
 import axios from 'axios'
 
-function JoinBlock() {
+function JoinBlock({onLogin}) {
   const [roomId, setRoomId] = useState('')
   const [userName, setUserName] = useState('')
+  const [isLoading, setLoading] = useState(false)
 
-  const onEnter = () => {
+  const onEnter = async () => {
     if(!roomId || !userName) {
       return alert("Error")
     }
-    axios.post('/rooms', {
+
+    setLoading(true)
+
+    await axios.post('/rooms', {
       roomId,
       userName
     })
+    onLogin
   }
 
   return (
     <div className="join-block">
       <input type="text" placeholder="Room ID" value={roomId} onChange={event => setRoomId(event.target.value)} />
       <input type="text" placeholder="Name" value={userName} onChange={event => setUserName(event.target.value)} />
-      <button className="btn btn-success" onClick={onEnter}>Enter</button>
+      <button disabled={isLoading} className="btn btn-success" onClick={onEnter}>{isLoading ? 'Loading...' : "Enter"}</button>
     </div>
   );
 }
