00706fc463a2c9f3b50ef82200e5306a5da64a55
Arhell <arhell333@gmail.com>
2021-05-21 01:03:45 +0300

    upd & fix

--- react/shop/src/Header.js
+++ react/shop/src/Header.js
@@ -1,6 +1,7 @@
 import logo from "./assets/img/pizza-logo.svg";
+import Button from "./Button";
 
-function Header () {
+const Header = () => {
   return (
     <div className="header">
       <div className="container">
@@ -12,7 +13,7 @@ function Header () {
           </div>
         </div>
         <div className="header__cart">
-          <a href="/cart.html" className="button button--cart">
+          <Button className="button--cart">
             <span>520 ₽</span>
             <div className="button__delimiter" />
             <svg
@@ -45,7 +46,7 @@ function Header () {
               />
             </svg>
             <span>3</span>
-          </a>
+          </Button>
         </div>
       </div>
     </div>
