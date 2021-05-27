af3126be975ff25144964d01cf93d9e42673543e
Arhell <arhell333@gmail.com>
2021-05-28 00:54:56 +0300

    update component

--- react/shop/src/App.js
+++ react/shop/src/App.js
@@ -1,14 +1,24 @@
 import { Header } from "./components";
 import {Cart, Home} from "./pages"
 import {Route} from "react-router-dom";
+import {useEffect, useState} from "react";
 
 function App() {
+  const [pizzas, setPizzas] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/db.json')
+      .then((response) => response.json())
+      .then(json => {
+        setPizzas(json.pizzas)
+    })
+  }, [])
 
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route path='/' component={Home} exact />
+        <Route path='/' render={() => <Home items={pizzas} />} exact />
         <Route path='/cart' component={Cart} exact />
       </div>
     </div>
