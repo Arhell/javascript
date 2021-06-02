61fa2bd093d584e00426ddc39f705c5e8f94e765
Arhell <arhell333@gmail.com>
2021-06-03 00:15:26 +0300

    update

--- react/shop/src/App.js
+++ react/shop/src/App.js
@@ -1,28 +1,38 @@
 import { Header } from "./components";
 import {Cart, Home} from "./pages"
 import {Route} from "react-router-dom";
-import {useEffect, useState} from "react";
+import {useEffect} from "react";
 import axios from "axios";
+import {useSelector, useDispatch} from "react-redux";
+
+import {setPizzas} from "./redux/actions/pizzas";
 
 function App() {
-  const [pizzas, setPizzas] = useState([])
+  const dispatch = useDispatch()
+  const {items} = useSelector(({pizzas, filters}) => {
+    return {
+      items: pizzas.items,
+      sortBy: filters.sortBy
+    }
+  })
 
   useEffect(() => {
     axios.get('http://localhost:3000/db.json')
       .then(({ data }) => {
-        setPizzas(data.pizzas)
+        dispatch(setPizzas(data.pizzas))
       })
+    // eslint-disable-next-line
   }, [])
 
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route path='/' render={() => <Home items={pizzas} />} exact />
+        <Route path='/' render={() => <Home items={items} />} exact />
         <Route path='/cart' component={Cart} exact />
       </div>
     </div>
   );
 }
 
-export default App;
+export default App
