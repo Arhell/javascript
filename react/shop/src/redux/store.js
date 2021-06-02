f08c7049cfc4a46c5a5fbc1e01c507b9bbaa835a
Arhell <arhell333@gmail.com>
2021-06-02 12:38:11 +0300

    add reducers

--- react/shop/src/redux/store.js
+++ react/shop/src/redux/store.js
@@ -1,16 +1,8 @@
 import { createStore } from "redux";
+import rootReducer from "./reducers";
 
-function counterReducer(state = { value: 0 }, action) {
-  switch (action.type) {
-    case 'counter/incremented':
-      return { value: state.value + 1 }
-    case 'counter/decremented':
-      return { value: state.value - 1 }
-    default:
-      return state
-  }
-}
+const store = createStore(rootReducer)
 
-const store = createStore(counterReducer)
+window.store = store
 
 export default store
