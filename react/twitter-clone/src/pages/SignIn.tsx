57015bc0396a7696c426e53b6ace55b4fbd45ed2
Arhell <arhell333@gmail.com>
2021-06-26 15:41:32 +0300

    add home page

--- react/twitter-clone/src/pages/SignIn.tsx
+++ react/twitter-clone/src/pages/SignIn.tsx
@@ -1,7 +1,109 @@
-function SignIn() {
-  return (
-    <div>
+import {Button, makeStyles, Typography} from "@material-ui/core";
+import TwitterIcon from '@material-ui/icons/Twitter';
+import SearchIcon from '@material-ui/icons/Search';
+import PeopleOutlineIcon from '@material-ui/icons/PeopleOutline';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
 
+const useStyles = makeStyles((theme) => ({
+  wrapper: {
+    display: 'flex',
+    height: '100vh',
+  },
+  blueSide: {
+    backgroundColor: '#71c9f8',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: '0 0 50%',
+    position: 'relative',
+    overflow: 'hidden',
+  },
+  blueSideBigIcon: {
+    position: 'absolute',
+    left: '50%',
+    top: '50%',
+    width: '150%',
+    height: '150%',
+    transform: 'translate(-50%, -50%)',
+  },
+  blueSideListInfo: {
+    position: 'relative',
+    listStyle: 'none',
+    padding: 0,
+    margin: 0,
+    width: 380,
+    '& h6': {
+      display: 'flex',
+      alignItems: 'start',
+      color: 'white',
+      fontWeight: 700,
+      fontSize: 20,
+    }
+  },
+  blueSideListInfoIcon: {
+    fontSize: 32,
+    marginRight: 15,
+  },
+  blueSideListInfoItem: {
+    marginBottom: 40,
+  },
+  loginSide: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: '0 0 50%',
+  },
+  loginSideTwitterIcon: {
+    fontSize: 48,
+    color: '#1da1f2',
+  },
+  loginSideWrapper: {
+    width: 380
+  },
+  loginSideTitle: {
+    fontWeight: 700,
+    fontSize: 32,
+    marginBottom: 45,
+    marginTop: 20,
+  }
+
+}))
+
+function SignIn() {
+  const classes = useStyles()
+
+  return (
+    <div className={classes.wrapper}>
+      <section className={classes.blueSide}>
+        <TwitterIcon color="primary" className={classes.blueSideBigIcon} />
+        <ul className={classes.blueSideListInfo}>
+          <li className={classes.blueSideListInfoItem}>
+            <Typography variant="h6">
+              <SearchIcon className={classes.blueSideListInfoIcon} /> Lorem ipsum dolor sit amet, consectetur.
+            </Typography>
+          </li>
+          <li className={classes.blueSideListInfoItem}>
+            <Typography variant="h6">
+              <PeopleOutlineIcon className={classes.blueSideListInfoIcon} /> Lorem ipsum dolor sit amet, consectetur adipisicing.
+            </Typography>
+          </li>
+          <li>
+            <Typography variant="h6">
+              <ChatBubbleOutlineIcon className={classes.blueSideListInfoIcon} /> Lorem ipsum dolor sit.
+            </Typography>
+          </li>
+        </ul>
+      </section>
+      <section className={classes.loginSide}>
+        <div className={classes.loginSideWrapper}>
+          <TwitterIcon color="primary" className={classes.loginSideTwitterIcon} />
+          <Typography variant="h4" className={classes.loginSideTitle}>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</Typography>
+          <Typography><strong>Lorem ipsum dolor sit amet</strong></Typography>
+          <br />
+          <Button style={{marginBottom: 20}} variant="contained" color="primary" fullWidth>Register</Button>
+          <Button variant="outlined" color="primary" fullWidth>Login</Button>
+        </div>
+      </section>
     </div>
   )
 }
