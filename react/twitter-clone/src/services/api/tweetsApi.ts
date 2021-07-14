f92c89108592a69db32d5a16b1bb47f38a48a688
Arhell <arhell333@gmail.com>
2021-07-14 17:52:15 +0300

    add server

--- react/twitter-clone/src/services/api/tweetsApi.ts
+++ react/twitter-clone/src/services/api/tweetsApi.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 import {TweetsState} from "../../store/ducks/tweets/contracts/state";
 
 export function fetchTweetsApi(): Promise<TweetsState['items']> {
-  return axios.get('https://trycode.pw/c/2OBQ1.json').then(({data}) => data)
+  return axios.get('/tweets').then(({data}) => data)
 }
